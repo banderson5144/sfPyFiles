39ec7da4e1271e33f0b2aaa60d6790d08b70a579
Bryan Anderson <banderson5144@gmail.com>
2022-01-05 16:52:19 -0500

    Added looping for queryMore

--- index.py
+++ index.py
@@ -16,45 +16,99 @@ sessionId = '00DXXXXXXXXXXXXXXXXXXX'
 # Set the Server URL by whatever means
 serverUrl = 'https://{yourMyDomain}.my.salesforce.com/services/Soap/u/52.0'
 
+# Assign Session Id to the SOAP Header Envelop
 client._default_soapheaders ={'SessionHeader':{"sessionId" : sessionId}} 
 
 # Create Salesforce Service from WSDL
 clientService = client.create_service( '{urn:partner.soap.sforce.com}SoapBinding', serverUrl)
 
-#Query the ContentVersion Ids first
-query_result = clientService.query("Select Id FROM ContentVersion ORDER BY ContentSize ASC")
+#Initial Query from Salesforce
+query_result = clientService.query("Select Id,ContentSize FROM ContentVersion ORDER BY ContentSize ASC")
 
-# print(query_result.body.result.records[0]['Id'])
+currRecSet = query_result.body.result.records
+currQryDone = query_result.body.result.done
+currQryLoc = query_result.body.result.queryLocator
 
-# Create Array of ContentVersion Ids
 cvIds = []
+cvSize = 0
 
-# Loop over query results to populate ContentVersion Ids
-for i in query_result.body.result.records:
-    cvIds.append(i.Id)
+# print(query_result.body.result.queryLocator)
 
-# Perform retrieve() API call in order get back multiple Files at once
-retrieve_result = clientService.retrieve('Title,FileExtension,VersionData','ContentVersion',cvIds)
+def retrieveFiles():
+    global cvIds
+    global cvSize
+    print('Downloading files from SF')
+    # Perform retrieve() API call in order get back multiple Files at once
+    retrieve_result = clientService.retrieve('Title,FileExtension,VersionData','ContentVersion',cvIds)
 
-# Since the Partner WSDL uses a generic sObject type
-# the Zeep SOAP client cannot parse the SOAP Response to strongly typed attributes
-# In order to access the values, it is by position in an array
-# OwnerId[0]
-# Title[1]
-# VersionData[2]
+    # Since the Partner WSDL uses a generic sObject type
+    # the Zeep SOAP client cannot parse the SOAP Response to strongly typed attributes
+    # In order to access the values, it is by position in an array
+    # OwnerId[0]
+    # Title[1]
+    # VersionData[2]
 
-for j in retrieve_result.body.result:
-    testDict = {} # Store the results in a dict so we can access easily instead of by position
-    for k in j._value_1:
-        testDict[k.tag] = k.text
-    file_content=base64.b64decode(testDict['{urn:sobject.partner.soap.sforce.com}VersionData'])
-    fileName = testDict['{urn:sobject.partner.soap.sforce.com}Title']
-    fileExt = testDict['{urn:sobject.partner.soap.sforce.com}FileExtension']
-    if fileExt is not None and fileExt != '':
-        try:
-            wholeName = 'files/' + fileName + '.' + fileExt
-            os.makedirs(os.path.dirname(wholeName), exist_ok=True)
-            with open(wholeName,"wb") as f:
-                f.write(file_content)
-        except Exception as e:
-            print(str(e))
+    for j in retrieve_result.body.result:
+        testDict = {} # Store the results in a dict so we can access easily instead of by position
+                
+        for k in j._value_1:
+            testDict[k.tag] = k.text
+
+        file_content=base64.b64decode(testDict['{urn:sobject.partner.soap.sforce.com}VersionData'])
+        fileName = testDict['{urn:sobject.partner.soap.sforce.com}Title']
+        fileExt = testDict['{urn:sobject.partner.soap.sforce.com}FileExtension']
+
+        if fileExt is not None and fileExt != '':
+            try:
+                wholeName = 'files/' + fileName + '.' + fileExt
+                os.makedirs(os.path.dirname(wholeName), exist_ok=True)
+                with open(wholeName,"wb") as f:
+                    f.write(file_content)
+            except Exception as e:
+                print(str(e))
+
+            if(len(currRecSet)>0):
+                cvIds = []
+                cvSize = 0
+                loopThroughSet()
+
+def sfQryMore():
+    global query_result
+    global currQryLoc
+    global currQryDone
+    global currRecSet
+    query_result = clientService.queryMore(currQryLoc)
+    currRecSet = query_result.body.result.records
+    currQryDone = query_result.body.result.done
+    currQryLoc = query_result.body.result.queryLocator
+    loopThroughSet()
+
+def loopThroughSet():
+    while len(currRecSet)>0:
+        global cvIds
+        global cvSize
+
+        myDict = {}
+
+        # Store the results in a dict so we can access easily instead of by position
+        for k in currRecSet[0]._value_1:
+            myDict[k.tag] = k.text
+
+        cvIds.append(currRecSet[0].Id)
+        cvSize+=int(myDict['{urn:sobject.partner.soap.sforce.com}ContentSize'])
+
+        del currRecSet[0]
+
+        #Pull down files from Salesforce if we hit 200 records or 500MB of files
+        if(len(cvIds)==200 or cvSize>=500000000):
+            retrieveFiles()
+            #If we have not hit the limits above but we have no records to loop over
+            #And the is still another query to run, pull them down from Salesforce
+        elif(len(currRecSet)==0 and currQryDone is False):
+            sfQryMore()
+            #If we did not hit either condition above, we are at the end and need
+            #and need to pull down the last set of files
+        elif len(currRecSet)==0:
+            retrieveFiles()
+
+loopThroughSet()
